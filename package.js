120ecaa30deb945e72a601da50a8f8166767a499
Marco de Jongh <marco.dejongh@gfk.com>
2014-05-22 10:40:58 +0200

    Weeding out the crap

--- package.js
+++ package.js
@@ -3,15 +3,15 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use('reactive-dict', ['client', 'server']);
-  api.use('deps', ['client', 'server']);
-  api.use('underscore', ['client', 'server']);
-  api.use('ejson', ['client', 'server']);
+  api.use('reactive-dict', ['client']);
+  api.use('deps', ['client']);
+  api.use('underscore', ['client']);
+  api.use('ejson', ['client']);
   api.use('jquery', 'client');
 
   // default ui manager
   // use unordered: true becuase of circular dependency
-
+  api.use('templating');
   // for helpers
   api.use('ui', 'client');
  
@@ -20,58 +20,51 @@ Package.on_use(function (api) {
   // depends on iron-router so it can register itself with
   // the router. But we still want to pull in the blaze-layout
   // package automatically when users add iron-router.
-  api.use('blaze-layout', 'client', {unordered: true});
-  api.use('deps-ext');
 
-  api.add_files('lib/utils.js', ['client', 'server']);
-  api.add_files('lib/route.js', ['client', 'server']);
-  api.add_files('lib/route_controller.js', ['client', 'server']);
-  api.add_files('lib/router.js', ['client', 'server']);
+  api.use('deps-ext');
+  api.add_files('lib/layout.js', 'client');
+  api.add_files('lib/overrides.js', 'client');
+
+  api.add_files('lib/utils.js', ['client']);
+  api.add_files('lib/route.js', ['client']);
+  api.add_files('lib/route_controller.js', ['client']);
+  api.add_files('lib/router.js', ['client']);
 
   api.add_files('lib/client/location.js', 'client');
   api.add_files('lib/client/router.js', 'client');
-  api.add_files('lib/client/wait_list.js', 'client');
-  api.add_files('lib/client/hooks.js', 'client');
   api.add_files('lib/client/route_controller.js', 'client');
   api.add_files('lib/client/ui/helpers.js', 'client');
 
-  api.add_files('lib/server/route_controller.js', 'server');
-  api.add_files('lib/server/router.js', 'server');
 
-  api.use('webapp', 'server');
-  Npm.depends({connect: '2.9.0'});
 
-  api.export('RouteController', ['client', 'server']);
-  api.export('Route', ['client', 'server']);
-  api.export('Router', ['client', 'server']);
+  api.export('RouteController', ['client']);
+  api.export('Route', ['client']);
+  api.export('Router', ['client']);
   api.export('IronLocation', 'client');
 
-  api.export('Utils', ['client', 'server'], {testOnly: true});
-  api.export('IronRouter', ['client', 'server'], {testOnly: true});
-  api.export('WaitList', 'client', {testOnly: true});
+  api.export('Utils', ['client'], {testOnly: true});
+  api.export('IronRouter', ['client'], {testOnly: true});
+
+  api.export('Layout', 'client', {testOnly: true});
 });
 
 Package.on_test(function (api) {
-  api.use('iron-router', ['client', 'server']);
-  api.use('tinytest', ['client', 'server']);
-  api.use('test-helpers', ['client', 'server']);
-  api.use('reactive-dict', ['client', 'server']);
+  api.use('iron-router', ['client']);
+  api.use('tinytest', ['client']);
+  api.use('test-helpers', ['client']);
+  api.use('reactive-dict', ['client']);
   api.use('deps-ext', 'client');
 
-  api.add_files('test/test_helpers.js', ['client', 'server']);
+  api.add_files('test/test_helpers.js', ['client']);
 
   // client and server
-  api.add_files('test/both/route.js', ['client', 'server']);
-  api.add_files('test/both/route_controller.js', ['client', 'server']);
-  api.add_files('test/both/router.js', ['client', 'server']);
-  api.add_files('test/both/utils.js', ['client', 'server']);
-
-  // server only
-  api.add_files('test/server/router.js', 'server');
+  api.add_files('test/both/route.js', ['client']);
+  api.add_files('test/both/route_controller.js', ['client']);
+  api.add_files('test/both/router.js', ['client']);
+  api.add_files('test/both/utils.js', ['client']);
 
   // client only
   api.add_files('test/client/mocks.js', 'client');
   api.add_files('test/client/router.js', 'client');
   api.add_files('test/client/route_controller.js', 'client');
-  api.add_files('test/client/wait_list.js', 'client');
 });
